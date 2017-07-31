fe9db4a0e36015e6dccc37ef1d92319ad9b84669
FranKarine <franciele.karine@hotmail.com>
2017-07-31 00:27:09 +0000

    Final project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   private
     def user_not_authorized
-      flash[:warning] = "Você não tem permissão para realizar essa ação."
+      flash[:warning] = "Sorry, you can not peform this action."
       redirect_to(request.referrer || root_path)
     end
 end
